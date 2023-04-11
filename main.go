8d71fe0d1c6eb577c2f09f27dbe2b9b21c727cbe
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-11 21:34:13 +0300

    Add strimzi operator.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -26,7 +27,35 @@ func main() {
 		// Namespace
 
 		// Strimzi operator
+		strimziKafkaOperator, err := helmv3.NewRelease(ctx, "strimzikafkaoperator", &helmv3.ReleaseArgs{
+			Chart:     pulumi.String("strimzi-kafka-operator"),
+			Namespace: namespace.Metadata.Name(),
+			RepositoryOpts: &helmv3.RepositoryOptsArgs{
+				Repo: pulumi.String("https://strimzi.io/charts/"),
+			},
+			// SkipCrds: pulumi.Bool(true),
+			// Values: pulumi.Map{
+			// 	"controller": pulumi.Map{
+			// 		"enableCustomResources": pulumi.Bool(false),
+			// 		"appprotect": pulumi.Map{
+			// 			"enable": pulumi.Bool(false),
+			// 		},
+			// 		"appprotectdos": pulumi.Map{
+			// 			"enable": pulumi.Bool(false),
+			// 		},
+			// 		"service": pulumi.Map{
+			// 			"extraLabels": appLabels,
+			// 		},
+			// 	},
+			// },
+			// Version: pulumi.String("latest"),
+		})
+		if err != nil {
+			return err
+		}
 
+		// Export some values for use elsewhere
+		ctx.Export("name", strimziKafkaOperator.Name)
 		// Strimzi operator
 
 		return nil
