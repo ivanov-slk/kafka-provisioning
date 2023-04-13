6f86c8eb86b6a0569f2badc35f87d785cbe5290d
Slav Ivanov <ivanov.slk@abv.bg>
2023-04-13 19:25:32 +0300

    Remove unneeded code.

--- main.go
+++ main.go
@@ -35,22 +35,6 @@ func main() {
 			RepositoryOpts: &helmv3.RepositoryOptsArgs{
 				Repo: pulumi.String("https://strimzi.io/charts/"),
 			},
-			// SkipCrds: pulumi.Bool(true),
-			// Values: pulumi.Map{
-			// 	"controller": pulumi.Map{
-			// 		"enableCustomResources": pulumi.Bool(false),
-			// 		"appprotect": pulumi.Map{
-			// 			"enable": pulumi.Bool(false),
-			// 		},
-			// 		"appprotectdos": pulumi.Map{
-			// 			"enable": pulumi.Bool(false),
-			// 		},
-			// 		"service": pulumi.Map{
-			// 			"extraLabels": appLabels,
-			// 		},
-			// 	},
-			// },
-			// Version: pulumi.String("latest"),
 		})
 		if err != nil {
 			return err
@@ -65,64 +49,12 @@ func main() {
 			File: "strimzi-kafka-cluster.yaml",
 		})
 
-		// // Ugly... why not just parse plain YAML?
-		// kafka, err := apiext.NewCustomResource(ctx, "kafka-cluster", &apiext.CustomResourceArgs{
-		// 	ApiVersion: pulumi.String("kafka.strimzi.io/v1beta2"),
-		// 	Kind:       pulumi.String("Kafka"),
-		// 	Metadata: &metav1.ObjectMetaArgs{
-		// 		Name:      pulumi.String("kafka-cluster"),
-		// 		Namespace: namespace.Metadata.Name(),
-		// 	},
-		// 	OtherFields: k8s.UntypedArgs{
-		// 		"spec": k8s.UntypedArgs{
-		// 			"kafka": k8s.UntypedArgs{
-		// 				"version":  "3.4.0",
-		// 				"replicas": 1,
-		// 				"listeners": []k8s.UntypedArgs{
-		// 					{
-		// 						"name": "plain",
-		// 						"port": 9092,
-		// 						"type": "internal",
-		// 						"tls":  false,
-		// 					}, {
-		// 						"name": "tls",
-		// 						"port": 9093,
-		// 						"type": "internal",
-		// 						"tls":  false,
-		// 					},
-		// 				},
-		// 				"config": k8s.UntypedArgs{
-		// 					"offsets.topic.replication.factor":         1,
-		// 					"transaction.state.log.replication.factor": 1,
-		// 					"transaction.state.log.min.isr":            1,
-		// 					"default.replication.factor":               1,
-		// 					"min.insync.replicas":                      1,
-		// 					"inter.broker.protocol.version":            "3.4",
-		// 				},
-		// 				"storage": map[string]string{"type": "ephemeral"},
-		// 			},
-		// 			"zookeeper": k8s.UntypedArgs{
-		// 				"replicas": 1,
-		// 				"storage":  map[string]string{"type": "ephemeral"},
-		// 			},
-		// 			"entityOperator": k8s.UntypedArgs{
-		// 				"topicOperator": map[string]string{},
-		// 				"userOperator":  map[string]string{},
-		// 			},
-		// 		},
-		// 	},
-		// })
-
 		if err != nil {
 			return err
 		}
 
 		kafka_cluster := kafka.GetResource("kafka.strimzi.io/v1beta2/Kafka", "kafka-cluster", "kafka-system") //.(*apiext.CustomResource)
-		// kafka_cluster.URN().OutputState
 		ctx.Export("Kafka Cluster", kafka_cluster.URN().ToStringOutput())
-		// ctx.Export("Kafka", kafka_cluster.Metadata.Name())
-		// ctx.Export("API Version", kafka_cluster.ApiVersion)
-		// ctx.Export("Custom Resource Type", kafka_cluster.Kind)
 		// Kafka and Zookeeper
 
 		return nil
